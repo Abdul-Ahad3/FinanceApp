12854f312be579cdc384ac12dace0d94721119f1
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2023-07-23 15:49:05 +0500

    Edited app environment

--- App.py
+++ App.py
@@ -1,4 +1,11 @@
 from tkinter import *
 
-window = Tk()
+window = Tk("500x500")
+label1 = Label(window, text="My App", font=('Arial',50,'bold'))
+label2 = Label(window, text="WELCOME", font=('Arial',50))
+label1.pack()   
+label2.pack()
+window.geometry("500x500")
+window.config(background="red")
+
 window.mainloop()
