97c9d16c77d11614cb6f3c960a3d2fd829023a66
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2023-07-24 10:58:38 +0500

    Changed background color

--- App.py
+++ App.py
@@ -1,11 +1,11 @@
 from tkinter import *
 
 window = Tk("500x500")
-label1 = Label(window, text="My App", font=('Arial',50,'bold'))
-label2 = Label(window, text="WELCOME", font=('Arial',50))
+label1 = Label(window, text="My App", font=('Arial',50,'bold'),bg='#FC4C4F')
+label2 = Label(window, text="WELCOME", font=('Arial',50),bg='#FC4C4F')
 label1.pack()   
 label2.pack()
 window.geometry("500x500")
-window.config(background="red")
+window.config(background="#FC4C4F")
 
 window.mainloop()
