1aeeae044f2efad200c1c174c503fdb061f6ba9e
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2023-07-26 14:52:02 +0500

    Changed component names

--- App.py
+++ App.py
@@ -1,22 +1,22 @@
 from tkinter import *
 
 def next():
-    newWin = Tk()
+    mainWin = Tk()
 
-    newWin.geometry("900x600")
-    newWin.config(background="#FC4C4F")
-    window.destroy()
+    mainWin.geometry("900x600")
+    mainWin.config(background="#FC4C4F")
+    welcome.destroy()
 
-window = Tk()
+welcome = Tk()
 
-label1 = Label(window, text="My App", font=('Arial',50,'bold'),bg='#FC4C4F')
-label2 = Label(window, text="WELCOME", font=('Arial',50),bg='#FC4C4F')
-welcome = Button(text='NEXT', font=('Arial',40), bg='black', fg='#FC4C4F', command=next)
+label1 = Label(welcome, text="My App", font=('Arial',50,'bold'),bg='#FC4C4F')
+label2 = Label(welcome, text="WELCOME", font=('Arial',50),bg='#FC4C4F')
+next = Button(text='NEXT', font=('Arial',40), bg='black', fg='#FC4C4F', command=next)
 
 label1.pack()   
 label2.pack()
-welcome.place(x=150, y=300)
-window.geometry("500x500")
-window.config(background="#FC4C4F")
+next.place(x=150, y=300)
+welcome.geometry("500x500")
+welcome.config(background="#FC4C4F")
 
-window.mainloop()
+welcome.mainloop()
