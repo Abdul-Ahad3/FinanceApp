a7600107b3e8649d8f45b6778a5ada96d1988501
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2023-07-26 14:49:22 +0500

    Worked on the main window

--- App.py
+++ App.py
@@ -1,9 +1,17 @@
 from tkinter import *
 
-window = Tk("500x500")
+def next():
+    newWin = Tk()
+
+    newWin.geometry("900x600")
+    newWin.config(background="#FC4C4F")
+    window.destroy()
+
+window = Tk()
+
 label1 = Label(window, text="My App", font=('Arial',50,'bold'),bg='#FC4C4F')
 label2 = Label(window, text="WELCOME", font=('Arial',50),bg='#FC4C4F')
-welcome = Button(text='NEXT', font=('Arial',40), bg='black', fg='#FC4C4F')
+welcome = Button(text='NEXT', font=('Arial',40), bg='black', fg='#FC4C4F', command=next)
 
 label1.pack()   
 label2.pack()
