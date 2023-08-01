53ebd3276afce91731f777519f0c513519ffd595
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2023-08-01 15:36:13 +0500

    Restarting the workon layout

--- App.py
+++ App.py
@@ -56,39 +56,6 @@ def nextB():
     fontMenu.add_command(label="Arial")
     fontMenu.add_command(label="Times New Roman")
     fontMenu.add_command(label="Calibri")
-
-    #Entry box for title of project
-    title = Entry(mainWin, font=('Arial', 30), width=20, bg='#0E9C7B')
-    title.insert(0, 'Enter title')
-    title.grid(row=1, column=2, columnspan=2, padx=(10, 10), pady=(10, 10))
-    def clear_entry(event):
-        title.delete(0, END)
-    title.bind('<FocusIn>', clear_entry)
-
-    #Account title label
-    accLabel = Label(mainWin, text='Account title', font=('Arial', 20, 'bold'), bg='#FC4C4F')
-    accLabel.grid(row=2, column=1)
-    
-    #Account title
-    accTitle = Entry(mainWin, font=('Arial', 30), width=20, bg='#0E9C7B')
-    accTitle.grid(row=2, column=2, columnspan=2, padx=(10, 10), pady=(10, 10))
-
-    #Transaction Label
-    transLabel = Label(mainWin, text='Choose Transaction', font=('Arial', 20, 'bold'), bg='#FC4C4F')
-    transLabel.grid(row=3, column=1)
-
-    #Frame to add the transactio menu
-    #tFrame = Frame(mainWin, width=200, height=50)
-    #tFrame.grid(row=3, column=2)
-    
-    #Transaction Menu
-    tMenu = Menu(mainWin, font=('Arial', 20))  # Example: Create a Transaction menu, add commands, and associate with the mainWin window.
-    tMenu.add_command(label="Transaction 1")
-    tMenu.add_command(label="Transaction 2")
-    tMenu.add_command(label="Transaction 3")
-    
-    cFrame = Frame(mainWin, width=200, height=200)
-    cFrame.grid(row=2, column=4, rowspan=3, columnspan=2)
     
     #Button to show preview
     preview = Button(mainWin, text='Show Preview', font=('Arial', 15), bg='black', fg='#FC4C4F')
