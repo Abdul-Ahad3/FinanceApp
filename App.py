e14fe42aa623ba8addd6d3d66aff89aaf9fd233f
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2023-08-07 11:30:15 +0500

    Added a new intermidiate window before the main one

--- App.py
+++ App.py
@@ -4,6 +4,67 @@ from tkinter import colorchooser
 from tkinter import filedialog
 import os
 
+def nexB():
+    mainWin = Tk()
+
+    #Function for opening a new file
+    def newP():
+        file = open("C:\\Users\\delll\\OneDrive\\Desktop\\MyApp\\Ledger\\file.txt", 'x')
+
+    #Function for opening an already existing file
+    def Open():
+        os.startfile(filedialog.askopenfilename())
+
+    #Function for exiting the window
+    def exit():
+        mainWin.destroy()
+
+    #Function for changing background color
+    def bGround():
+        mainWin.config(bg=colorchooser.askcolor()[1])
+    
+
+    menuBar = Menu(mainWin, font=("Arial", 30)) #Main menu bar
+    i=10
+
+    #File drop down menu
+    fileMenu = Menu(menuBar, tearoff=0, font=("Arial", 10))
+    menuBar.add_cascade(label="File", menu=fileMenu)
+    fileMenu.add_command(label="New", command=newP)
+    fileMenu.add_command(label="Open", command=Open)
+    fileMenu.add_command(label="Save")#, command=save)
+    fileMenu.add_command(label="Save As")#, command=SaveAs)
+    fileMenu.add_command(label="Exit", command=exit)
+
+    #Edit drop down menu
+    editMenu = Menu(menuBar, tearoff=0, font=("Arial", 10))
+    menuBar.add_cascade(label="Edit", menu=editMenu)
+    editMenu.add_command(label="Background", command=bGround)
+    
+    #Font Size menu in Edit Menu
+    fsMenu = Menu(editMenu, tearoff=0, font=('Arial', 10))
+    editMenu.add_cascade(label='Font Size', menu=fsMenu)
+    while(i<=50):
+        fsMenu.add_command(label=str(i))
+        i+=2
+    
+    #Font menu in Edit Menu
+    fontMenu = Menu(editMenu, tearoff=0, font=('Arial', 10))
+    editMenu.add_cascade(label='Font', menu=fontMenu)
+    fontMenu.add_command(label="Arial")
+    fontMenu.add_command(label="Times New Roman")
+    fontMenu.add_command(label="Calibri")
+
+
+    button = Button(mainWin, text='+ Add Ledger', font=('Arial', 20), bg='black', fg='#FC4C4F', width=30, command =nextB)
+    button.place(relx=0.3, rely=0.2)
+    
+    mainWin.geometry("1100x600")
+    mainWin.config(background='#FC4C4F', menu=menuBar)
+    welcome.destroy()
+    mainWin.mainloop()
+
+
 #Code for the main window (after next button is pressed)
 def nextB():
     #Function for opening a new file
@@ -157,14 +218,13 @@ def nextB():
 
     mainWin.geometry("1130x600")
     mainWin.config(background="#FC4C4F", menu=menuBar)
-    welcome.destroy()
 
 #Welcome window
 welcome = Tk()
 
 label1 = Label(welcome, text="My App", font=('Arial',50,'bold'),bg='#FC4C4F')
 label2 = Label(welcome, text="WELCOME", font=('Arial',50),bg='#FC4C4F')
-nextButton = Button(text='NEXT', font=('Arial',40), bg='black', fg='#FC4C4F', command=nextB)
+nextButton = Button(text='NEXT', font=('Arial',40), bg='black', fg='#FC4C4F', command=nexB)
 
 label1.pack()   
 label2.pack()
